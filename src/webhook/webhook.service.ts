d5e26c54c798876f8602e524e864db44db437d86
romanthapa2 <thaparoman970@gmail.com>
2025-04-07 21:02:18 +0545

    feat : adds pr to queue

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { QueueService } from '../queue/queue.service';
 
 @Injectable()
-export class WebhookService {}
+export class WebhookService {
+  constructor(private readonly queueService: QueueService) {}
+
+  async enqueuePR(payload: any) {
+    const { number, base } = payload.pull_request;
+    const owner = base.repo.owner.login;
+    const repo = base.repo.name;
+
+    await this.queueService.addPRToQueue(owner, repo, number);
+  }
+}
