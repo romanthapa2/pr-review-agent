9f84b5989b06f5294012f6fc6cb5af9c95f1811f
romanthapa2 <thaparoman970@gmail.com>
2025-04-07 16:16:14 +0545

    created module for webhook  github and queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AiModule } from './ai/ai.module';
+import { GithubModule } from './github/github.module';
+import { QueueModule } from './queue/queue.module';
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
-  imports: [AiModule],
+  imports: [AiModule, GithubModule, QueueModule, WebhookModule],
   controllers: [AppController],
   providers: [AppService],
 })
